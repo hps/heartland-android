07d87597baa5045b91f067c09bf499ce6381240e
Jay Barria <jaybarria@gmail.com>
2021-03-15 21:44:09 +0800

    Update DeviceListener.java
    Update TerminalInfo to use Parent Class instead

--- library/src/main/java/com/heartlandpaymentsystems/library/terminals/DeviceListener.java
+++ library/src/main/java/com/heartlandpaymentsystems/library/terminals/DeviceListener.java
@@ -4,7 +4,8 @@ import java.util.HashSet;
 
 import android.bluetooth.BluetoothDevice;
 
-import com.heartlandpaymentsystems.library.terminals.entities.TerminalInfo;
+//import com.heartlandpaymentsystems.library.terminals.entities.TerminalInfo;
+import com.tsys.payments.library.domain.TerminalInfo;
 
 public interface DeviceListener {
     void onBluetoothDeviceList(HashSet<BluetoothDevice> deviceList);
