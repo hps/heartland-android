72d646bc85723d5a4b591ccee22da22a43c8853b
Philip White <philip.white@e-hps.com>
2022-03-22 15:46:49 -0400

    Updates for the example app to show more functionality of the SDK with bluetooth connection and test transactions.

---
+++ example-app/src/main/java/com/example/app/BluetoothActivity.java
@@ -0,0 +1,225 @@
+package com.example.app;
+
+import static com.example.app.Dialogs.hideProgress;
+import static com.example.app.Dialogs.showProgress;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.heartlandpaymentsystems.library.terminals.DeviceListener;
+import com.heartlandpaymentsystems.library.terminals.entities.TerminalInfo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class BluetoothActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "BluetoothActivity";
+
+    private TextView connectionStatusTextView;
+    private Button connectButton;
+    private Button reconnectButton;
+    private String bluetoothDeviceName = null;
+    private String bluetoothDeviceAddress = null;
+    private BluetoothDevice previousSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+        connectionStatusTextView = findViewById(R.id.c2x_connection_status);
+        connectButton = findViewById(R.id.connect_button);
+        reconnectButton = findViewById(R.id.reconnect_button);
+
+        connectButton.setOnClickListener(this);
+        reconnectButton.setOnClickListener(this);
+
+        //check for previous bluetooth device
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SAVED_PREFS, MODE_PRIVATE);
+        bluetoothDeviceName = sharedPreferences.getString(MainActivity.BLUETOOTH_NAME, null);
+        Log.e("test", "saved BT device: " + bluetoothDeviceName);
+        if (bluetoothDeviceName != null) {
+            updateReconnectButton(true, bluetoothDeviceName);
+        }
+
+        if (MainActivity.c2XDevice.isConnected()) {
+            updateConnectionStatus("Connected - " + bluetoothDeviceName, false);
+        }
+
+        MainActivity.c2XDevice.setDeviceListener(new DeviceListener() {
+            @Override
+            public void onBluetoothDeviceFound(BluetoothDevice bluetoothDevice) {
+                Log.d(TAG, "onBluetoothDeviceFound - " + bluetoothDevice.getName());
+            }
+
+            @Override
+            public void onBluetoothDeviceList(final HashSet<BluetoothDevice> deviceList) {
+                hideProgress();
+                // receive list of available bluetooth devices
+                // present to user for selection/confirmation
+                if (deviceList == null || deviceList.isEmpty()) {
+                    return;
+                }
+
+                Log.e(TAG, "onBluetoothDeviceList - size " + deviceList.size());
+
+                //Use this flag is the list of bluetooth keep popping up with the list of bluetooth.
+                if (previousSelected != null) {
+                    return;
+                }
+
+                // receive list of available bluetooth devices
+                // present to user for selection/confirmation
+                final List<BluetoothDevice> list = new ArrayList<BluetoothDevice>(deviceList);
+                final List<String> btList = new ArrayList<String>();
+
+                for (BluetoothDevice dev : list) {
+                    if(dev.getName() != null) {
+                        btList.add(dev.getName().trim());
+                    }
+                }
+
+                //This is a dialog to show the list of devices
+                //Here we use a dialog with a callback and a listener but you can use your own user selection
+                AlertDialog d = Dialogs.showListDialog("Available devices",
+                        BluetoothActivity.this, btList.toArray(new String[0]),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(final DialogInterface dialogInterface, int i) {
+                                BTSelection.getInstance().Selection(list.get(i), new BTSelection.SelectionCallback() {
+                                    @Override
+                                    public void onSelection(BluetoothDevice btDevice) {
+                                        //keep track of current device info
+                                        bluetoothDeviceName = btDevice.getName();
+                                        bluetoothDeviceAddress = btDevice.getAddress();
+                                        dialogInterface.dismiss();
+                                        if (selectionListener != null) {
+                                            previousSelected = btDevice;
+                                            selectionListener.onSelection(btDevice);
+                                        }
+                                    }
+                                });
+                            }
+                        });
+            }
+
+            @Override
+            public void onConnected(final TerminalInfo terminalInfo) {
+                Log.d(TAG, "onConnected");
+
+                //update connection status
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateConnectionStatus("Connected - " + terminalInfo.getSerialNumber(),
+                                false);
+                    }
+                });
+
+                //save the bluetooth data
+                SharedPreferences.Editor editor = getSharedPreferences(MainActivity.SAVED_PREFS, MODE_PRIVATE).edit();
+                editor.putString(MainActivity.BLUETOOTH_NAME, bluetoothDeviceName);
+                editor.putString(MainActivity.BLUETOOTH_ADDRESS, bluetoothDeviceAddress);
+                editor.commit();
+            }
+
+            @Override
+            public void onError(Error error) {
+                Log.e(TAG, "onError - " + error.toString());
+
+                //update connection status
+                updateConnectionStatus("Error - " + error.getMessage(), true);
+            }
+
+            @Override
+            public void onTerminalInfoReceived(TerminalInfo terminalInfo) {
+                Log.d(TAG, "onTerminalInfoReceived");
+            }
+        });
+    }
+
+    /**
+     * Updated the connection status TextView and the connect Button.
+     * @param status Text status to be displayed.
+     * @param connectButtonEnabled Whether the connect button should be enabled/disabled.
+     */
+    private void updateConnectionStatus(String status, boolean connectButtonEnabled) {
+        //update connection status
+        connectionStatusTextView.setText(status);
+        //update the connect button
+        connectButton.setEnabled(connectButtonEnabled);
+        //update the reconnect button
+        reconnectButton.setEnabled(connectButtonEnabled);
+    }
+
+    /**
+     * Update the reconnect button text and enabled status.
+     * @param enabled
+     * @param deviceName
+     */
+    private void updateReconnectButton(boolean enabled, String deviceName) {
+        //update enabled
+        reconnectButton.setEnabled(enabled);
+        //update the text
+        reconnectButton.setText(enabled ? getString(R.string.bluetooth_reconnect) + " (" + deviceName + ")" :
+                getString(R.string.bluetooth_reconnect));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.connect_button:
+                MainActivity.c2XDevice.initialize();
+
+                //update connection status
+                updateConnectionStatus("Scanning", false);
+                showProgress(this, "Scanning", "Scanning for Bluetooth devices");
+                break;
+            case R.id.reconnect_button:
+                SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SAVED_PREFS, MODE_PRIVATE);
+                String previousDevice = sharedPreferences.getString(MainActivity.BLUETOOTH_NAME, null);
+                String previousAddress = sharedPreferences.getString(MainActivity.BLUETOOTH_ADDRESS, null);
+                MainActivity.c2XDevice.connect(previousAddress);
+
+                Log.d(TAG, "bluetooth device connect - " + previousDevice);
+
+                //update connection status
+                updateConnectionStatus("Connecting - " + previousDevice,
+                        false);
+                break;
+        }
+    }
+
+    private interface ISelection {
+        void onSelection(BluetoothDevice device);
+    }
+
+    final ISelection selectionListener = new ISelection() {
+        @Override
+        public void onSelection(BluetoothDevice device) {
+            try {
+                //Connect the selected device.
+                MainActivity.c2XDevice.connect(device);
+                updateConnectionStatus("Connecting - " + device.getName(),
+                        false);
+            } catch (Exception e) {
+                e.printStackTrace();
+                updateConnectionStatus("An exception occurred.", true);
+            }
+        }
+    };
+}
---
+++ example-app/src/main/java/com/example/app/CardEntryActivity.java
@@ -0,0 +1,310 @@
+package com.example.app;
+
+import static com.example.app.Dialogs.hideProgress;
+import static com.example.app.Dialogs.showProgress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.heartlandpaymentsystems.library.CardFragmentInteractionListener;
+import com.heartlandpaymentsystems.library.controller.TokenService;
+import com.heartlandpaymentsystems.library.entities.Card;
+import com.heartlandpaymentsystems.library.entities.Token;
+import com.heartlandpaymentsystems.library.terminals.TransactionListener;
+import com.heartlandpaymentsystems.library.terminals.c2x.CreditSaleBuilder;
+import com.heartlandpaymentsystems.library.terminals.entities.CardholderInteractionRequest;
+import com.heartlandpaymentsystems.library.terminals.entities.CardholderInteractionResult;
+import com.heartlandpaymentsystems.library.terminals.entities.TerminalResponse;
+import com.heartlandpaymentsystems.library.terminals.enums.TransactionStatus;
+
+import java.math.BigDecimal;
+
+public class CardEntryActivity extends AppCompatActivity implements CardFragmentInteractionListener, View.OnClickListener {
+
+    private static final String TAG = "CardEntryActivity";
+
+    private Button creditSaleManualButton;
+    private TextView resultTextView;
+    private ProgressDialog mProgressDialog;
+    private static MainActivity.TransactionState transactionState = MainActivity.TransactionState.None;
+    private static String transactionId;
+    private static String transactionResult;
+    private AlertDialog mAlertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_entry);
+
+        MainActivity.c2XDevice.setTransactionListener(transactionListener);
+
+        creditSaleManualButton = findViewById(R.id.creditsale_manual_button);
+        resultTextView = findViewById(R.id.tokenizeResult);
+        final EditText cardNo = findViewById(com.heartlandpaymentsystems.library.R.id.card_no_edt);
+        final EditText cardExpDate = findViewById(com.heartlandpaymentsystems.library.R.id.card_exp_date_edt);
+        final EditText cardExpYear = findViewById(com.heartlandpaymentsystems.library.R.id.card_exp_yr_edt);
+        final EditText cardCvv = findViewById(com.heartlandpaymentsystems.library.R.id.card_cvv_edt);
+        final Button submitBtn = findViewById(com.heartlandpaymentsystems.library.R.id.submit);
+
+        findViewById(R.id.creditsale_manual_button).setOnClickListener(this);
+
+        updateTransactionStatus();
+
+        if (isConnected()) {
+            submitBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (view == submitBtn) {
+
+                        hideKeyboard(CardEntryActivity.this, submitBtn);
+
+                        try {
+                            showProgressDialog();
+                            TokenService tokenService = new TokenService(MainActivity.PUBLIC_KEY);
+                            final Card card = new Card();
+                            card.setNumber(cardNo.getText().toString());
+                            card.setExpMonth(Integer.valueOf(cardExpDate.getText().toString()));
+                            card.setExpYear(Integer.valueOf(cardExpYear.getText().toString()));
+                            card.setCvv(cardCvv.getText().toString());
+
+                            tokenService.getToken(card, new TokenService.TokenCallback() {
+                                @Override
+                                public Token onComplete(Token response) {
+                                    hideProgressDialog();
+                                    Log.d(TAG, "token service response: " + response.toString());
+                                    if (response == null || response.getError() != null) {
+                                        onTokenFailure(response.getError().getMessage());
+                                    } else {
+                                        onTokenSuccess(response);
+                                    }
+                                    return response;
+                                }
+                            });
+                        }catch (NumberFormatException e) {
+                            e.printStackTrace();
+                            hideProgressDialog();
+                        }
+                    }
+                }
+
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Please check your internet connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateTransactionStatus() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView transactionStatus = findViewById(R.id.transaction_status);
+                switch (transactionState) {
+                    case None:
+                    case Processing:
+                        transactionStatus.setText(transactionState.toString());
+                        break;
+                    case Complete:
+                        transactionStatus.setText(transactionState + " - " + transactionResult + " - " + transactionId);
+                        break;
+                }
+                creditSaleManualButton.setEnabled(transactionState != MainActivity.TransactionState.Processing);
+            }
+        });
+    }
+
+    public boolean isConnected() {
+        boolean connected = false;
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            connected = networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
+            return connected;
+        } catch (Exception e) {
+            Log.e("Connectivity Exception", e.getMessage());
+        }
+        return connected;
+    }
+
+    public void hideKeyboard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (view != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog.show(this, getString(com.heartlandpaymentsystems.library.R.string.loading_msg),
+                    getString(com.heartlandpaymentsystems.library.R.string.tokenizing));
+            mProgressDialog.setCancelable(false);
+        } else {
+            mProgressDialog.show();
+        }
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onTokenSuccess(Token response) {
+        resultTextView.setText(response.getTokenValue());
+    }
+
+    @Override
+    public void onTokenFailure(String response) {
+        resultTextView.setText(response);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.creditsale_manual_button:
+                String cardNo = ((EditText) findViewById(R.id.card_no_edt)).getText().toString();
+                String month = ((EditText) findViewById(R.id.card_exp_date_edt)).getText().toString();
+                String year = ((EditText) findViewById(R.id.card_exp_yr_edt)).getText().toString();
+                String cvv = ((EditText) findViewById(R.id.card_cvv_edt)).getText().toString();
+                int expMonth = 0;
+                int expYear = 0;
+                try {
+                    expMonth = Integer.parseInt(month);
+                    expYear = Integer.parseInt(year);
+                }catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    showAlertDialog(getString(R.string.error), getString(R.string.error_enter_valid_expiration));
+                }
+
+                //check for missing fields
+                if (cardNo.isEmpty() || cvv.isEmpty()) {
+                    showAlertDialog(getString(R.string.error), getString(R.string.error_enter_valid_card));
+                    return;
+                }
+
+                Card card = new Card();
+                card.setNumber(cardNo);
+                card.setExpMonth(expMonth);
+                card.setExpYear(expYear);
+                card.setCvv(cvv);
+                CreditSaleBuilder builder = new CreditSaleBuilder(MainActivity.c2XDevice);
+                builder.setAmount(new BigDecimal("10.00"));
+                builder.setCreditCard(card);
+                try {
+                    builder.execute();
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    showAlertDialog(getString(R.string.error), getString(R.string.error_enter_valid_card));
+                }
+                break;
+        }
+    }
+
+    private TransactionListener transactionListener = new TransactionListener() {
+        @Override
+        public void onStatusUpdate(TransactionStatus transactionStatus) {
+            Log.d(TAG, "onStatusUpdate - " + transactionStatus.toString());
+            transactionState = MainActivity.TransactionState.Processing;
+            updateTransactionStatus();
+            if(!transactionStatus.name().equals(TransactionStatus.NONE.name())) {
+                showProgress(CardEntryActivity.this, "Status", transactionStatus.name());
+            }
+        }
+
+        @Override
+        public void onCardholderInteractionRequested(CardholderInteractionRequest cardholderInteractionRequest) {
+            showProgress(CardEntryActivity.this, "Status", "Processing...");
+            Log.d(TAG, "onCardholderInteractionRequested");
+            // prompt user for action
+            CardholderInteractionResult result;
+            switch (cardholderInteractionRequest.getCardholderInteractionType()) {
+                case EMV_APPLICATION_SELECTION:
+                    String[] applications =
+                            cardholderInteractionRequest.getSupportedApplications();
+                    // prompt user to select desired application
+                    // send result
+                    result = new CardholderInteractionResult(
+                            cardholderInteractionRequest.getCardholderInteractionType()
+                    );
+                    result.setSelectedAidIndex(0);
+                    MainActivity.c2XDevice.sendCardholderInteractionResult(result);
+                    break;
+                case FINAL_AMOUNT_CONFIRMATION:
+                    // prompt user to confirm final amount
+                    result = new CardholderInteractionResult(
+                            cardholderInteractionRequest.getCardholderInteractionType()
+                    );
+                    result.setFinalAmountConfirmed(true);
+                    MainActivity.c2XDevice.sendCardholderInteractionResult(result);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        @Override
+        public void onTransactionComplete(TerminalResponse transaction) {
+            Log.d(TAG, "onTransactionComplete - " + transaction.toString());
+            hideProgress();
+            transactionId = transaction.getTransactionId();
+            transactionResult = transaction.getDeviceResponseCode();
+            transactionState = MainActivity.TransactionState.Complete;
+            updateTransactionStatus();
+
+            showAlertDialog(getString(R.string.transaction_complete), Dialogs.constructTransactionMessage(transaction));
+        }
+
+        @Override
+        public void onError(Error error) {
+            Log.e(TAG, "onError - " + error.getMessage());
+            hideProgress();
+            transactionResult = error.getMessage();
+            transactionState = MainActivity.TransactionState.Complete;
+            updateTransactionStatus();
+
+            showAlertDialog(getString(R.string.transaction_error), error.getMessage());
+        }
+    };
+
+    private void showAlertDialog(String title, String message) {
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            Log.e(TAG, "showAlertDialog - dialog is already showing");
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                mAlertDialog.dismiss();
+            }
+        });
+        mAlertDialog = builder.create();
+        mAlertDialog.setCancelable(false);
+        mAlertDialog.show();
+    }
+
+    private void hideAlertDialog() {
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            mAlertDialog.hide();
+        }
+    }
+}
